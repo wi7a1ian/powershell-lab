6098a7f88fe9050dcc16b651b620c1178bc69d1b
Wit <witalian.zajac@gmail.com>
2018-11-21 09:34:49 +0100

    updated remote script

--- Remote.ps1
+++ Remote.ps1
@@ -8,36 +8,31 @@ cls
 # Enable remote access and configure WinRM
 #Enable-PSRemoting -Force #-SkipNetworkProfileCheck
 #winrm quickconfig
-##winrm set winrm/config/client '@{TrustedHosts="MALINOWSKA"}'
+##winrm set winrm/config/client '@{TrustedHosts="Machine1"}'
 
 # Set trusted hosts on both client and server
-#Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "MALINOWSKA"
-#Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "Wit-Komputer"
+#Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "Machine1"
+#Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value "Machine2"
 # Validate
 #Get-Item WSMan:\localhost\Client\TrustedHosts
 
 #Restart-Service WinRM
 
 # Testing the connection
-Test-WsMan MALINOWSKA #-UseSSL
-Test-WsMan Wit-Komputer #-UseSSL
+Test-WsMan Machine1 #-UseSSL
+Test-WsMan Machine2 #-UseSSL
 # should display anything or error if failed to connect to remote WinRM
 
-#. .\Take-ScreenShot.ps1
-#. "$PSScriptRoot\Take-ScreenShot.ps1"
-#Import-Module "$PSScriptRoot\Take-ScreenShot.psm1" -Force
-#Take-ScreenShot -screen -file "C:\image.png" -imagetype png
+$credential = Get-Credential -Credential Machine1\SomeUsername
+#Invoke-Command -ComputerName Machine1 -ScriptBlock { Get-ChildItem C:\ } -Credential $credential
+#Invoke-Command -ComputerName Machine1 -ScriptBlock { Take-ScreenShot -screen -file "C:\image.png" -imagetype png } -Credential $credential
+#Enter-PSSession -ComputerName Machine1 -Credential Machine2\SomeUsername
 
-$credential = Get-Credential -Credential malinowska\wit
-#Invoke-Command -ComputerName MALINOWSKA -ScriptBlock { Get-ChildItem C:\ } -Credential $credential
-#Invoke-Command -ComputerName MALINOWSKA -ScriptBlock { Take-ScreenShot -screen -file "C:\image.png" -imagetype png } -Credential $credential
-#Enter-PSSession -ComputerName MALINOWSKA -Credential Wit-Komputer\Wit
+$s = New-PSSession -ComputerName Machine1 -Credential $credential
 
-$s = New-PSSession -ComputerName MALINOWSKA -Credential $credential
-
-Test-Path \\WIT-KOMPUTER\Shared\Take-ScreenShot.psm1
-Invoke-Command -Session $s -ScriptBlock { Test-Path \\WIT-KOMPUTER\Shared\Take-ScreenShot.psm1 }
+Test-Path \\Machine2\Shared\Take-ScreenShot.psm1
+Invoke-Command -Session $s -ScriptBlock { Test-Path \\Machine2\Shared\Take-ScreenShot.psm1 }
 # TODO: double hop issue http://blogs.msdn.com/b/clustering/archive/2009/06/25/9803001.aspx
-#Invoke-Command -Session $s -ScriptBlock { Import-Module "\\WIT-KOMPUTER\Shared\Take-ScreenShot.psm1" -Force }
-#Invoke-Command -Session $s -ScriptBlock { Take-ScreenShot -screen -file "\\WIT-KOMPUTER\Shared\screencap.png" -imagetype png }
+#Invoke-Command -Session $s -ScriptBlock { Import-Module "\\Machine2\Shared\Take-ScreenShot.psm1" -Force }
+#Invoke-Command -Session $s -ScriptBlock { Take-ScreenShot -screen -file "\\Machine2\Shared\screencap.png" -imagetype png }
 Remove-PSSession -session $s
